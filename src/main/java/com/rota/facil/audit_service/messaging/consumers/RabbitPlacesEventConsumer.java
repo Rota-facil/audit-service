7d0510900261f1deca192cbb89dcc2516c076e04
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2026-04-28 13:27:36 -0300

    feat: add audit data persist and retrieve

--- src/main/java/com/rota/facil/audit_service/messaging/consumers/RabbitPlacesEventConsumer.java
+++ src/main/java/com/rota/facil/audit_service/messaging/consumers/RabbitPlacesEventConsumer.java
@@ -1,6 +1,8 @@
 package com.rota.facil.audit_service.messaging.consumers;
 
+import com.rota.facil.audit_service.business.AuditService;
 import com.rota.facil.audit_service.messaging.dto.receive.AuditEventReceive;
+import com.rota.facil.audit_service.messaging.mappers.AuditEventMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -8,7 +10,8 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class RabbitPlacesEventConsumer {
-    // inejtar service que vai converter isso para uma entidade e vai persistir
+    private final AuditService auditService;
+    private final AuditEventMapper auditEventMapper;
 
     @RabbitListener(queues = {
             "${rabbitmq.audit.institution.created.queue}",
@@ -19,6 +22,6 @@ public class RabbitPlacesEventConsumer {
             "${rabbitmq.audit.boarding.deleted.queue}"
     })
     public void handlerPlacesEvent(AuditEventReceive auditEventReceive) {
-
+        auditService.register(auditEventMapper.map(auditEventReceive));
     }
 }
